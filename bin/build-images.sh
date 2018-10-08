575d807768f596d5713d14b39e1a211e908bcf10
domac <domacode@gmail.com>
2018-10-08 16:55:37 +0800

    add cerebro

--- bin/build-images.sh
+++ bin/build-images.sh
@@ -4,9 +4,14 @@ WORKSPACE=$(cd $(dirname $0)/; pwd)
 
 version="0.0.1"
 dockerimages=( kenl-base kenl-kafka-base kenl-kafka-broker kenl-zookeeper )
-
 for name in ${dockerimages[@]}
 do  
-    #cd $WORKSPACE/../docker/${name} && docker build -t ${name}:${version} .
     docker pull domacli/${name}:${version}
+done
+
+
+thirdparty=( kenl-elasticsearch kenl-logstash kenl-kibana kenl-cerebro )
+for name in ${thirdparty[@]}
+do  
+    cd $WORKSPACE/../docker/${name} && docker build  .
 done
